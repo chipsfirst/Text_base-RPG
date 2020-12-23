b041303f13b0d26274d0f27abb65baedcdea987c
Kartashov Vladimir <chipsfirst1@mail.ru>
2020-12-23 14:28:40 +0400

    Colorama added

--- quest.py
+++ quest.py
@@ -2,14 +2,14 @@ import random
 import colorama
 from colorama import Fore, Back, Style
 from data_maps import data_maps
-colorama.init()
+colorama.init(autoreset=True)
 
 
 client_data = []
 steps = []
-character = input(Fore.BLUE+"Добро пожаловать в Новогодний квест! Как вас зовут?: ")
+character = input(f"{Back.RED}Добро пожаловать в Новогодний квест! Как вас зовут?: ")
 client_data.append(character)
-location = input("Откуда вы к нам?: ")
+location = input(f"{Back.LIGHTGREEN_EX}Откуда вы к нам?: ")
 client_data.append(location)
 print("------------------------------------------------------------------------------------")
 question1 = input('Отличные новости, ' + client_data[0] + '! Двери нашей резиденции откроются совсем скоро. '
