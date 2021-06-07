14ff98f31e661b62040e9a8583460054698a3480
Kartashov Vladimir <chipsfirst1@mail.ru>
2021-06-07 12:42:44 +0300

    version 1.4.2

--- main_game.py
+++ main_game.py
@@ -2,12 +2,13 @@ import colorama, random
 from colorama import Fore, Back
 import Character
 import os, sys
-from locations import map_1, map_2
+from locations import map_1, map_2, map_3, map_4
+from functions import Dragon1, Dragon2, Dragon3, Dragon4
 
 colorama.init(autoreset=True)
 import time
 
-dice_roll = random.randint(1,20)
+dice_roll = random.randint(1,6)
 #def print_slow(str):
 #   for letter in str:
 #        sys.stdout.write(letter)
@@ -35,24 +36,45 @@ print("Это камень-указатель. На нем четыре стре
            "3) Направо - Древние топи.\n"
            "4) Налево - Ледяные пустоши.\n"
            "5) Переписать карту на листок и положить в инвентарь. \n"
-           "6) Бросить кубик\n")
+           "6) Сражение со случайным монстром. \n"
+           "7) Бросить кубик\n")
 
 
 path = []
-ways = path.append(input("Input your way:"))
+ways = path.append(input("Выберите путь:"))
 for ways in path:
     if ways == "1":
         print(map_1.name)
         print(map_1.description)
         print(map_1.puzzle)
-        break
+        ways = path.append(input("Выберите путь:"))
     elif ways == "2":
         print(map_2.name)
         print(map_2.description)
         print(map_2.puzzle)
-        break
+        ways = path.append(input("Выберите путь:"))
+    elif ways == "3":
+        print(map_3.name)
+        print(map_3.description)
+        print(map_3.puzzle)
+        ways = path.append(input("Выберите путь:"))
+    elif ways == "4":
+        print(map_4.name)
+        print(map_4.description)
+        print(map_4.puzzle)
+        ways = path.append(input("Выберите путь:"))
+    elif ways == "5":
+        Character.Inventory.append(input("Записать: "))
+        print(Character.Inventory)
+        ways = path.append(input("Выберите путь:"))
     elif ways == "6":
+        monsters = [str(Dragon1.name + Dragon1.description), str(Dragon2.name + Dragon2.description),
+                    str(Dragon3.name + Dragon3.description), str(Dragon4.name + Dragon4.description)]
+        print("На вас нападает " + random.choice(monsters))
+        ways = path.append(input("Выберите путь:"))
+    elif ways == "7":
         print(dice_roll)
+        ways = path.append(input("Выберите путь:"))
     else:
-        ways = path.append(input("Input your way:"))
+        ways = path.append(input("Выберите путь:"))
 
