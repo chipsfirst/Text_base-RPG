1d9be1da8a50762c2021d6ca3b351dce7f72f375
Kartashov <chipsfirst1@mail.ru>
2022-11-15 16:47:04 +0300

    Еще правочки

--- Character.py
+++ Character.py
@@ -1,14 +1,37 @@
-class Character:
+import random
 
-    def __init__(self, name, age):
+def rolling(num_dice):
+    roll_results = []
+    for _ in range(num_dice):
+        roll = random.randint(1, 6)
+        roll_results.append(roll)
+    return roll_results
+
+class Player():
+
+    def __init__(self, name, race, game_class, initiative, weapon, armory, inventory, money, ac):
         self.name = name
-        self.age = age
+        self.race = race
+        self.game_class = game_class
+        self.initiative = initiative
+        self.weapon = weapon
+        self.armory = armory
+        self.inventory = inventory
+        self.money = money
+        self.ac = ac
 
-    def password(self):
-        return bin(int(self))
+    race = []
+    name = []
+    game_class = []
+    initiative = 3
+    attack = 3
+    ac = 13
+    weapon = []
+    armory = []
+    inventory = []
+    money = 100
 
-
-class Points:
+class Character_Point():
     def __init__(self, strange, dexterity, constitution, intelligence, wisdom, charisma):
         self.strange = strange
         self.dexterity = dexterity
@@ -17,29 +40,32 @@ class Points:
         self.wisdom = wisdom
         self.charisma = charisma
 
-    def strange_up(strange):
-        strange += 1
-        return strange
+    strange = 8
+    dexterity = 8
+    constitution = 8
+    intelligence = 8
+    wisdom = 8
+    charisma = 8
 
-    def dex_up(dexterity):
-        dexterity += 1
-        return dexterity
+# def print_slow(str):
+#    for letter in str:
+#         sys.stdout.write(letter)
+#         sys.stdout.flush()
+#         time.sleep(0.2)
 
-    def const_up(constitution):
-        constitution += 1
-        return constitution
-
-    def intel_up(intelligence):
-        intelligence += 1
-        return intelligence
-
-    def wisdom_up(wisdom):
-        wisdom += 1
-        return wisdom
-
-    def charisma_up(charisma):
-        charisma += 1
-        return charisma
+def Char_Asset():
+    print("Вас зовут " + str(Player.name) + ". Вы -" + str(*Player.race) + " класса " + str(*Player.game_class) + ".")
+    print("Сила равна " + str(Character_Point.strange) + ".")
+    print("Ловкость равна " + str(Character_Point.dexterity) + ".")
+    print("Телосложение равно " + str(Character_Point.constitution) + ".")
+    print("Мудрость равна " + str(Character_Point.wisdom) + ".")
+    print("Интеллект равен " + str(Character_Point.intelligence) + ".")
+    print("Харизма равна " + str(Character_Point.charisma) + ".")
+    print("Ваша атака равна " + str(Player.attack) + ".")
+    print("Ваша инициатива равна " + str(Player.initiative) + ".")
+    print("Ваш класс доспеха равен " + str(Player.ac) + ".")
+    return("Удачного путешествия!")
 
 
-Inventory = []
+
+
--- main_game.py
+++ main_game.py
@@ -1,61 +1,15 @@
+import Character
 import random
 
 def rolling(num_dice):
     roll_results = []
-
     for _ in range(num_dice):
         roll = random.randint(1, 6)
-
         roll_results.append(roll)
-
     return roll_results
 
-class Player():
-
-    def __init__(self, name, race, game_class, initiative, weapon, armory, inventory, money):
-        self.name = name
-        self.race = race
-        self.game_class = game_class
-        self.initiative = initiative
-        self.weapon = weapon
-        self.armory = armory
-        self.inventory = inventory
-        self.money = money
-
-    race = []
-    name = []
-    game_class = []
-    initiative = 3
-    attack = 3
-    weapon = []
-    armory = []
-    inventory = []
-    money = 100
-
-class Character_Point():
-    def __init__(self, strange, dexterity, constitution, intelligence, wisdom, charisma):
-        self.strange = strange
-        self.dexterity = dexterity
-        self.constitution = constitution
-        self.intelligence = intelligence
-        self.wisdom = wisdom
-        self.charisma = charisma
-
-    strange = 8
-    dexterity = 8
-    constitution = 8
-    intelligence = 8
-    wisdom = 8
-    charisma = 8
-
-# def print_slow(str):
-#    for letter in str:
-#         sys.stdout.write(letter)
-#         sys.stdout.flush()
-#         time.sleep(0.2)
-
 print("Приветствую тебя, путник!\n")
-Player.name = input(str("Введите имя своего персонажа: \n"))
+Character.Player.name = input(str("Введите имя своего персонажа: \n"))
 choose_race = print("Выберите расу своего персонажа.\n"
                     "Раса влияет на бонусы к характеристикам.\n"
                      "1) Человек.\n"
@@ -68,40 +22,41 @@ choose = []
 player_race = choose.append(input("Введите номер выбранной расы или напишите 'Выход': "))
 for player_race in choose:
     if player_race == "1":
-        Player.race.append("человек")
-        print(*Player.race)
-        Character_Point.strange += 2
-        print("Вы " + str(*Player.race) + ", и получаете бонус к силе. Теперь значение вашей силы равно " + str(Character_Point.strange) + "."
+        Character.Player.race.append("человек")
+        print(*Character.Player.race)
+        Character.Character_Point.strange += 2
+        print("Вы " + str(*Character.Player.race) + ", и получаете бонус к силе. Теперь значение вашей силы равно " +
+              str(Character.Character_Point.strange) + "."
               )
     elif player_race == "2":
-        Player.race.append("дварф")
-        Character_Point.constitution += 2
-        print("Вы " + str(*Player.race) + ", и получаете бонус к телосложению. Теперь значение вашего телосложения равно " + str(
-            Character_Point.constitution) + "."
+        Character.Player.race.append("дварф")
+        Character.Character_Point.constitution += 2
+        print("Вы " + str(*Character.Player.race) + ", и получаете бонус к телосложению. Теперь значение вашего телосложения равно " + str(
+            Character.Character_Point.constitution) + "."
               )
     elif player_race == "3":
-        Player.race.append("эльф")
-        Character_Point.dexterity += 2
-        print("Вы " + str(*Player.race) + ", и получаете бонус к ловкости. Теперь значение вашей ловкости равно " + str(
-            Character_Point.dexterity) + "."
+        Character.Player.race.append("эльф")
+        Character.Character_Point.dexterity += 2
+        print("Вы " + str(*Character.Player.race) + ", и получаете бонус к ловкости. Теперь значение вашей ловкости равно " + str(
+            Character.Character_Point.dexterity) + "."
               )
     elif player_race == "4":
-        Player.race.append("полурослик")
-        Character_Point.charisma += 3
-        print("Вы " + str(*Player.race) + ", и получаете бонус к харизме. Теперь значение вашей харизмы равно " + str(
-            Character_Point.charisma) + "."
+        Character.Player.race.append("полурослик")
+        Character.Character_Point.charisma += 3
+        print("Вы " + str(*Character.Player.race) + ", и получаете бонус к харизме. Теперь значение вашей харизмы равно " + str(
+            Character.Character_Point.charisma) + "."
               )
     elif player_race == "5":
-        Player.race.append("тифлинг")
-        Character_Point.wisdom += 2
-        print("Вы " + str(*Player.race) + ", и получаете бонус к мудрости. Теперь значение вашего телосложения равно " + str(
-            Character_Point.wisdom) + "."
+        Character.Player.race.append("тифлинг")
+        Character.Character_Point.wisdom += 2
+        print("Вы " + str(*Character.Player.race) + ", и получаете бонус к мудрости. Теперь значение вашего телосложения равно " + str(
+            Character.Character_Point.wisdom) + "."
               )
     elif player_race == "6":
-        Player.race.append("гном")
-        Character_Point.constitution += 2
-        print("Вы " + str(*Player.race) + ", и получаете бонус к телосложению. Теперь значение вашего телосложения равно " + str(
-            Character_Point.constitution) + "."
+        Character.Player.race.append("гном")
+        Character.Character_Point.constitution += 2
+        print("Вы " + str(*Character.Player.race) + ", и получаете бонус к телосложению. Теперь значение вашего телосложения равно " + str(
+            Character.Character_Point.constitution) + "."
               )
     else:
         print("Вы ввели неверный номер.")
@@ -119,51 +74,40 @@ classplayer = []
 player_class = classplayer.append(input("Введите номер выбранного класса: "))
 for player_class in classplayer:
     if player_class == "1":
-        Player.game_class.append("воин")
-        Player.attack += 2
-        print("Вы " + str(*Player.game_class) + ", и получаете бонус к атаке. Теперь значение вашей атаки равно " +
-              str(Player.attack) + ".")
+        Character.Player.game_class.append("воин")
+        Character.Player.attack += 2
+        print("Вы " + str(*Character.Player.game_class) + ", и получаете бонус к атаке. Теперь значение вашей атаки равно " +
+              str(Character.Player.attack) + ".")
     elif player_class == "2":
-        Player.game_class.append("следопыт")
-        Character_Point.dexterity += 2
-        print("Вы " + str(*Player.game_class) + ", и получаете бонус к ловкости. "
+        Character.Player.game_class.append("следопыт")
+        Character.Character_Point.dexterity += 2
+        print("Вы " + str(*Character.Player.game_class) + ", и получаете бонус к ловкости. "
                                                 "Теперь значение вашей ловкости равно " +
-              str(Character_Point.dexterity) + ".")
+              str(Character.Character_Point.dexterity) + ".")
     elif player_class == "3":
-        Player.game_class.append("плут")
-        Player.initiative += 2
-        print("Вы " + str(*Player.game_class) + ", и получаете бонус к инициативе. Теперь её значение равно " + str(
-            Player.initiative) + "."
+        Character.Player.game_class.append("плут")
+        Character.Player.initiative += 2
+        print("Вы " + str(*Character.Player.game_class) + ", и получаете бонус к инициативе. Теперь её значение равно " + str(
+            Character.Player.initiative) + "."
               )
     elif player_class == "4":
-        Player.game_class.append("волшебник")
-        Character_Point.wisdom += 2
-        print("Вы " + str(*Player.game_class) + ", и получаете бонус к мудрости. Теперь значение вашей мудрости равно "
-              + str(Character_Point.wisdom) + ".")
+        Character.Player.game_class.append("волшебник")
+        Character.Character_Point.wisdom += 2
+        print("Вы " + str(*Character.Player.game_class) + ", и получаете бонус к мудрости. Теперь значение вашей мудрости равно "
+              + str(Character.Character_Point.wisdom) + ".")
     elif player_class == "5":
-        Player.game_class.append("варвар")
-        Character_Point.strange += 2
-        print("Вы " + str(*Player.game_class) + ", и получаете бонус к силе. Теперь значение вашей силы равно " + str(
-            Character_Point.strange) + ".")
+        Character.Player.game_class.append("варвар")
+        Character.Character_Point.strange += 2
+        print("Вы " + str(*Character.Player.game_class) + ", и получаете бонус к силе. Теперь значение вашей силы равно " + str(
+            Character.Character_Point.strange) + ".")
     elif player_class == "6":
-        Player.game_class.append("бард")
-        Character_Point.charisma += 2
-        print("Вы " + str(*Player.game_class) + ", и получаете бонус к харизме. Теперь значение вашей "
-                                                "харизмы равно " + str(Character_Point.charisma) + ".")
+        Character.Player.game_class.append("бард")
+        Character.Character_Point.charisma += 2
+        print("Вы " + str(*Character.Player.game_class) + ", и получаете бонус к харизме. Теперь значение вашей "
+                                                "харизмы равно " + str(Character.Character_Point.charisma) + ".")
     else:
         print("Вы ввели неверный номер.")
         player_class = classplayer.append(input("Введите номер выбранного класса: "))
 
 
-def Char_Asset():
-    print("Вас зовут " + str(Player.name) + ". Вы -" + str(*Player.race) + " класса " + str(*Player.game_class) + ".")
-    print("Сила равна " + str(Character_Point.strange) + ".")
-    print("Ловкость равна " + str(Character_Point.dexterity) + ".")
-    print("Телосложение равно " + str(Character_Point.constitution) + ".")
-    print("Мудрость равна " + str(Character_Point.wisdom) + ".")
-    print("Интеллект равен " + str(Character_Point.intelligence) + ".")
-    print("Харизма равна " + str(Character_Point.charisma) + ".")
-    return("Удачного путешествия!")
-
-print(Char_Asset())
-
+print(Character.Char_Asset())
